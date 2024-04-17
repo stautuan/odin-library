e406389b1a412b1446eae823dcdc05466e62805a
stautuan <stautuan89@gmail.com>
2024-04-17 15:43:19 -0400

    Added a book placeholder image when an image url is not provided

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-"use script";
+'use script';
 
-const bookContainer = document.querySelector(".bookshelf");
-const dialog = document.querySelector("dialog");
-const addBtn = document.querySelector(".btn--add");
-const newBtn = document.querySelector(".btn--new");
-const closeBtn = document.querySelector(".btn--close");
-const remove = document.querySelector(".delete");
+const bookContainer = document.querySelector('.bookshelf');
+const dialog = document.querySelector('dialog');
+const addBtn = document.querySelector('.btn--add');
+const newBtn = document.querySelector('.btn--new');
+const closeBtn = document.querySelector('.btn--close');
+const remove = document.querySelector('.delete');
 
 const myLibrary = [];
 
@@ -18,22 +18,22 @@ function Book(title, author, image) {
 }
 
 function clearAllFields() {
-  inputTitle = "";
-  inputAuthor = "";
-  inputImageUrl = "";
+  inputTitle = '';
+  inputAuthor = '';
+  inputImageUrl = '';
 }
 
 // Function that takes user's input and stores them into the myLibrary array
 function addBookToLibrary() {
-  const inputTitle = document.getElementById("title").value;
-  const inputAuthor = document.getElementById("author").value;
-  const inputImageUrl = document.getElementById("image").value;
+  const inputTitle = document.getElementById('title').value;
+  const inputAuthor = document.getElementById('author').value;
+  const inputImageUrl = document.getElementById('image').value;
 
   let book = new Book(inputTitle, inputAuthor, inputImageUrl);
   myLibrary.push(book);
 }
 
-addBtn.addEventListener("click", (e) => {
+addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   addBookToLibrary();
 
@@ -42,11 +42,15 @@ addBtn.addEventListener("click", (e) => {
     const markup = `
           <div class="book">
             <span class="ribbon-wrap">
-              <span class="ribbon ribbon--unread"></span>
+              <span class="ribbon"></span>
             </span>
             <img
               class="book__cover"
-              src="${book.image}"
+              src="${
+                book.image
+                  ? book.image
+                  : 'https://bookstoreromanceday.org/wp-content/uploads/2020/08/book-cover-placeholder.png'
+              }"
               alt="${book.title} book cover"
             />
 
@@ -58,15 +62,15 @@ addBtn.addEventListener("click", (e) => {
           </div>
   `;
     clearAllFields();
-    bookContainer.insertAdjacentHTML("beforeend", markup);
+    bookContainer.insertAdjacentHTML('beforeend', markup);
   });
 });
 
 // Opens and closes the dialog
-newBtn.addEventListener("click", () => {
+newBtn.addEventListener('click', () => {
   dialog.showModal();
 });
-closeBtn.addEventListener("click", () => {
+closeBtn.addEventListener('click', () => {
   dialog.close();
 });
 
@@ -76,8 +80,8 @@ closeBtn.addEventListener("click", () => {
  * any event within the bookContainer (the parent) will be
  * checked if it contains the clicked target
  */
-bookContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("delete")) {
-    e.target.closest(".book").remove();
+bookContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete')) {
+    e.target.closest('.book').remove();
   }
 });
