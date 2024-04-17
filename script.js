f0c844c1ffb27acb83b52524c73611f577979c6b
stautuan <stautuan89@gmail.com>
2024-04-17 14:28:22 -0400

    Added an event handler to remove books from the shelf

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-'use script';
+"use script";
+
+const bookContainer = document.querySelector(".bookshelf");
+const dialog = document.querySelector("dialog");
+const addBtn = document.querySelector(".btn--add");
+const newBtn = document.querySelector(".btn--new");
+const closeBtn = document.querySelector(".btn--close");
+const remove = document.querySelector(".delete");
 
 const myLibrary = [];
 
@@ -6,32 +13,31 @@ function Book(title, author, image) {
   this.title = title;
   this.author = author;
   this.image = image;
+
+  function toggleReadStatus() {}
 }
 
-// Function that takes user's input and stores new books into the myLibrary array
+function clearAllFields() {
+  inputTitle = "";
+  inputAuthor = "";
+  inputImageUrl = "";
+}
+
+// Function that takes user's input and stores them into the myLibrary array
 function addBookToLibrary() {
-  const inputTitle = document.getElementById('title').value;
-  const inputAuthor = document.getElementById('author').value;
-  const inputImageUrl = document.getElementById('image').value;
+  const inputTitle = document.getElementById("title").value;
+  const inputAuthor = document.getElementById("author").value;
+  const inputImageUrl = document.getElementById("image").value;
 
   let book = new Book(inputTitle, inputAuthor, inputImageUrl);
   myLibrary.push(book);
 }
 
-function clearAllFields() {
-  inputTitle = '';
-  inputAuthor = '';
-  inputImageUrl = '';
-}
-
-const addBtn = document.querySelector('.btn--add');
-
-addBtn.addEventListener('click', (e) => {
+addBtn.addEventListener("click", (e) => {
   e.preventDefault();
   addBookToLibrary();
 
-  // Function that loops through the array and displays each book on the page
-  const bookContainer = document.querySelector('.bookshelf');
+  // Function that loops through the array and renders the book on the page
   myLibrary.forEach((book) => {
     const markup = `
           <div class="book">
@@ -52,19 +58,26 @@ addBtn.addEventListener('click', (e) => {
           </div>
   `;
     clearAllFields();
-    bookContainer.insertAdjacentHTML('beforeend', markup);
+    bookContainer.insertAdjacentHTML("beforeend", markup);
   });
 });
 
 // Opens and closes the dialog
-const newBtn = document.querySelector('.btn--new');
-const closeBtn = document.querySelector('.btn--close');
-const dialog = document.querySelector('dialog');
-newBtn.addEventListener('click', () => {
+newBtn.addEventListener("click", () => {
   dialog.showModal();
 });
-closeBtn.addEventListener('click', () => {
+closeBtn.addEventListener("click", () => {
   dialog.close();
 });
 
-// Removes the book from the library
+/**
+ * Removes the book from the library using event delegation
+ *
+ * any event within the bookContainer (the parent) will be
+ * checked if it contains the clicked target
+ */
+bookContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.closest(".book").remove();
+  }
+});
